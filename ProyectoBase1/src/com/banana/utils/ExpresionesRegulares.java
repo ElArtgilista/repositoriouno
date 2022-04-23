ccafbf1bec3b0db308d28ab7b3c7b127f992109e
kawor <kawor@LAPTOP-1TGN1CL9>
2022-04-23 09:43:32 -0500

    Carga actualizada 2

--- ProyectoBase1/src/com/banana/utils/ExpresionesRegulares.java
+++ ProyectoBase1/src/com/banana/utils/ExpresionesRegulares.java
@@ -5,6 +5,8 @@ public class ExpresionesRegulares {
 	public static void main(String args[])
 	{
 		System.out.println("Demo");
+		System.out.println("Demo2");
+		System.out.println("Demo3");
 	}
 
 }
